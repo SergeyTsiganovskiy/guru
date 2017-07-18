6304f013342d5b4ea08b5f56b63d7503bffb0e68
tsyganovskiy <tsyganovskiySS@gmail.com>
2017-07-18 20:42:00 +0300

    рефакторинг: добавлен TestScript01

---
+++ selenium-bank/src/test/java/com/guru99/TestScript01.java
@@ -0,0 +1,55 @@
+package com.guru99;
+
+/*
+ * Set of test script for testing Website Guru99 Bank
+ * The test scripts is developed using Selenium Framework
+ *
+ */
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+/**
+ *
+ * @author Krishna Rungta
+ *        Test Script 01
+ *        **************
+ *        Test Steps
+ *
+1)  Go to http://www.demo.guru99.com/V4/
+2) Enter valid UserId
+3) Enter valid Password
+4) Click Login
+ */
+
+public class TestScript01 {
+
+
+
+  public static void main(String[] args) throws Exception {
+
+    //Setup Firefox driver
+    System.setProperty("webdriver.gecko.driver", "D:\\geckodriver.exe");
+    WebDriver driver = new FirefoxDriver();
+    String baseUrl = "http://www.demo.guru99.com/V4/";
+
+    // launch Firefox and direct it to the Base URL
+    driver.get(baseUrl);
+
+
+    // Enter username
+    driver.findElement(By.name("uid")).sendKeys("mngr88256");
+
+    // Enter Password
+    driver.findElement(By.name("password")).sendKeys("ysyvYsA");
+
+    // Click Login
+    driver.findElement(By.name("btnLogin")).click();
+
+
+  }
+
+
+}
+
+
--- selenium-bank/src/test/java/com/guru99/_1_lesson/expert/LoginTest.java
+++
@@ -1,4 +0,0 @@
-package com.guru99._1_lesson.expert;
-
-public class LoginTest {
-}
